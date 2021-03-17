a49a1fa5e0febf16ebe4aa78b27ac0f1772747d1
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-17 18:07:38 +0530

    Create Exercise 1.2: Spikey.java

---
+++ BJP5/Chapter 1: Introduction to Java Programming/Exercise 1.2: Spikey.java
@@ -0,0 +1,11 @@
+public class Spikey {
+    public static void main(String[] args) {
+        System.out.println("  \\/"); // two backslashes ie \\ prints one \
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+    }
+    
+}
