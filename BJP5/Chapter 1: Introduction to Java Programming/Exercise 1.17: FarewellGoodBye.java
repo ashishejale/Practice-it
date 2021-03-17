1e90addf1b5a6c70956c63efec5d74aa0ff16cc2
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-17 23:34:16 +0530

    Create Exercise 1.17: FarewellGoodBye.java

---
+++ BJP5/Chapter 1: Introduction to Java Programming/Exercise 1.17: FarewellGoodBye.java
@@ -0,0 +1,13 @@
+public class FarewellGoodBye {
+    public static void main(String[] args) {
+        printFirstThreeLines();
+        System.out.println("good night!\nIt's time, to go,\nand I'll be out of sight!\n");
+        printFirstThreeLines();
+        System.out.println("take care!\nI'll say, goodbye,\nthat's neither here nor there!\n");
+        printFirstThreeLines();
+        System.out.println("see you later!\nI hope, you think,\nI'm a lover, not a hater!");
+    }
+    public static void printFirstThreeLines() {
+        System.out.println("Farewell,\ngoodbye,\nau revoir,");
+    }
+}
