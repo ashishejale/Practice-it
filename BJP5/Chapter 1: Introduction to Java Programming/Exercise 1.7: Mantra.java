a5e7eec08b786243b46757d6b6ed7b56bd20043b
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-17 19:12:42 +0530

    Create Exercise 1.7: Mantra.java

---
+++ BJP5/Chapter 1: Introduction to Java Programming/Exercise 1.7: Mantra.java
@@ -0,0 +1,12 @@
+public class Mantra {
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();  
+    }
+    public static void myMethod() {
+        System.out.println("There's one thing every coder must understand:");
+        System.out.println("The System.out.println command.");
+        System.out.println("");
+        // or use this single line print statemnt which makes use of \n ie. System.out.println("There's one thing every coder must understand:\nThe System.out.println command.\n");
+    }
+}
