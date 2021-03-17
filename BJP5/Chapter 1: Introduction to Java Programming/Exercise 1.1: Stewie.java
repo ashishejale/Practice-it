302994edfc370c850162928a1b518e0a4a9a4ac1
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-17 17:59:40 +0530

    Create Exercise 1.1: Stewie.java

---
+++ BJP5/Chapter 1: Introduction to Java Programming/Exercise 1.1: Stewie.java
@@ -0,0 +1,8 @@
+public class Stewie {
+    public static void main(String[] args) {
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"); // twice he number of \ required to print 
+                                                                            //as previous two \ ie \\ escapes one \ this new escaped \ is counted into the next \\
+        }
+}
