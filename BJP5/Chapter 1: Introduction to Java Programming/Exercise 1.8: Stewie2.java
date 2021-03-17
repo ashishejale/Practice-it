f9f8afa1ce3058cb7c9a6f3c0ee9d5fb63fa1bad
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-17 19:15:05 +0530

    Create Exercise 1.8: Stewie2.java

---
+++ BJP5/Chapter 1: Introduction to Java Programming/Exercise 1.8: Stewie2.java
@@ -0,0 +1,15 @@
+public class Stewie2 {
+    public static void main(String[] args) {
+       
+        System.out.println("//////////////////////");
+        myMethod();
+        myMethod();
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+    public static void myMethod(){
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"); //one \ escapes one \ ,so need twice the number of \ to print the required 
+    }
+}
