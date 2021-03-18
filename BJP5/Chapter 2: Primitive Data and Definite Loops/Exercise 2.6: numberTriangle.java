e0aa2bc4fece6121d32496f913744db5d280d10f
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-18 21:50:14 +0530

    Create Exercise 2.6: numberTriangle.java

---
+++ BJP5/Chapter 2: Primitive Data and Definite Loops/Exercise 2.6: numberTriangle.java
@@ -0,0 +1,6 @@
+for(int i = 1; i <= 7; i++) { //use i to count number as well as the line number
+    for(int j = 1; j <= i; j++) { //use j to print the number as many times as the number of line
+        System.out.print(i);
+    }
+    System.out.println();
+}
