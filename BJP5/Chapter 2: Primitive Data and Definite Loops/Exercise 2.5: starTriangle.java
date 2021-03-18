f0f5637eec9c752a1d2e30337b85a399bbe80ae2
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-18 21:43:39 +0530

    Create Exercise 2.5: starTriangle.java

---
+++ BJP5/Chapter 2: Primitive Data and Definite Loops/Exercise 2.5: starTriangle.java
@@ -0,0 +1,6 @@
+for(int i = 0; i < 5; i++) {  //for counting line use i
+    for(int j = 0; j <= i; j++) {   //use j to print as many starts as the line number ie. 2 stars in the second line
+        System.out.print("*");
+    }
+    System.out.println();
+}
