8530e1f71adc527681813b9215705908216b732a
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-18 21:57:25 +0530

    Create Exercise 2.7: spacedNumbers.java

---
+++ BJP5/Chapter 2: Primitive Data and Definite Loops/Exercise 2.7: spacedNumbers.java
@@ -0,0 +1,6 @@
+for(int i = 1; i <= 5; i++) {
+    for(int j = 5; j > i; j--) {
+        System.out.print(" "); // print as many spaces as there are number
+    }
+    System.out.println(i); // then print the number
+}
