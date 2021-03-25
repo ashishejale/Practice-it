1bbfe71dc2ee84a0f83e8cb4d651bf83fda9bf44
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-03-25 17:41:01 +0530

    Create Exercise 2.14: nestedNumbers3.java

---
+++ BJP5/Chapter 2: Primitive Data and Definite Loops/Exercise 2.14: nestedNumbers3.java
@@ -0,0 +1,8 @@
+for(int i = 0; i < 4; i++) { //for 4 lines
+    for(int j = 9; j > 0; j--) { // to generate number
+        for(int k = j; k > 0; k--) { //for j repetitions of each number
+            System.out.print(j);
+        }
+    }
+    System.out.println();
+}
